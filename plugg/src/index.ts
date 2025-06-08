546985eed4c1b19679877559071d349c3e6bc002
2backside <marioluizalves.luiz@proton.me>
2025-06-07 21:58:24 -0300

    refactor: remove unused middleware and simplify index file

--- plugg/src/index.ts
+++ plugg/src/index.ts
@@ -1,13 +1,7 @@
-import express, {Request, Response} from "express";
+import express from "express";
 import { routes } from "./routes/index"
 
 const app = express();
-app.use(express.json());
-
-app.get("/", (req: Request, res: Response) => {
-    res.send('OIIII SAFADAO');
-});
-
 routes(app)
 
 app.listen(3000, () => {

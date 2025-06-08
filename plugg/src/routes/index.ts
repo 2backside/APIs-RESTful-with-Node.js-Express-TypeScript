99d72fb646489493d11c0d677d45e122b7d99b67
2backside <marioluizalves.luiz@proton.me>
2025-06-08 18:19:59 -0300

    Remove unnecessary files and code related to the plugg application, including package.json, controllers, routes, and TypeScript configuration. This cleanup eliminates unused dependencies and simplifies the project structure.

--- plugg/src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { userRoutes } from "./users_routes";
-
-export const routes = (app: express.Express) => {
-    app.use(express.json())
-    app.use(userRoutes);
-}
